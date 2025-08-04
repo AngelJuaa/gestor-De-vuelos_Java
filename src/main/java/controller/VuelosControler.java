2dca8842f773f45091f20ca3808d720091728ee7
angel arellano <AngelJuaa>
2025-08-04 10:41:20 -0600

    first commit

---
+++ src/main/java/controller/VuelosControler.java
@@ -0,0 +1,69 @@
+package controller;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+import modelo.Vuelos;
+import view.VueloView;
+
+public class VuelosControler {
+
+    private List<Vuelos> vuelos = new ArrayList<>();
+    private Vuelos model;
+    private VueloView view;
+
+    public VuelosControler(Vuelos model, VueloView view){
+        this.model = model;
+        this.view = view;
+    }
+
+    public void CRUDVuelo(){
+        view.CRUDVuelo(model);
+    }
+
+    public void createVuelo() {
+        view.createVuelo(model);
+    }
+
+    public void readVuelo(){
+        view.readVuelo(model);
+    }
+
+    public void updateVuelo(){
+        view.updateVuelo(model);
+    }
+
+    public boolean deleteVuelo(String codigo){
+        return vuelos.removeIf(v -> v.getCodigo().equalsIgnoreCase(codigo));
+    }
+
+    public Vuelos buscarVuelo(String codigo) {
+        return vuelos.stream()
+                     .filter(v -> v.getcodigo().equalsIgnoreCase(codigo))
+                     .findFirst()
+                     .orElse(null);
+    }
+
+    public List<Vuelos> vuelosOrdenados() {
+        List<Vuelos> ordenados = new ArrayList<>(vuelos);
+        ordenados.sort((Vuelos v1, Vuelos v2) -> v2.getfecha_completa().compareTo(v1.getfecha_completa()));
+        return ordenados;
+    }
+
+    public Vuelos vueloPasajeros() {
+        return vuelos.stream()
+                     .min((v1, v2) -> v1.getnumeropasajeros().compareTo(v2.getnumeropasajeros()))
+                     .orElse(null);
+    }
+
+    public Vuelos llegada(){
+        List<Vuelos> ordenados = vuelosOrdenados();
+        return ordenados.isEmpty() ? null : ordenados.get(0);
+    }
+
+    public void guardarVuelo(String codigo, String origen, String destino, String hora, Date fecha_completa, Integer numeroPasajeros){
+        Vuelos nuevo = new Vuelos(codigo, origen, destino, hora, fecha_completa, numeroPasajeros);
+        vuelos.add(nuevo);
+        view.updateTable(nuevo);
+    }
+}
---
+++ src/main/java/mx/edu/itses/aaja/vuelosexamen/VuelosExamen.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.edu.itses.aaja.vuelosexamen;
+
+import controller.VuelosControler;
+import modelo.Vuelos;
+import view.VueloView;
+
+/**
+ *
+ * @author Angel
+ */
+public class VuelosExamen {
+
+    static Vuelos model;
+    static VueloView view;
+    static VuelosControler controler;
+    public static void main(String[] args){
+        
+        model = new Vuelos(codigo, origen, destino, hora, fecha_completa, numeroPasajeros);
+        view = new VueloView();
+        controler = new VuelosControler(model, view); 
+        
+        controler.CRUDVuelo(model);
+    }
+}
